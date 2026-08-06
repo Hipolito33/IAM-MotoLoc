2e674360416fb0a38fdb4baf4df2de6bc5a2984a
Arthur Hipólito <hipolitoarthur3622@gmail.com>
2026-08-06 19:53:21 -0300

    feat: implementa cadastro de motos com Prisma

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const app = require('./src/app');
 
 app.listen(3000, () => {
     console.log('Servidor iniciado na porta 3000.');
-})
+});
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+
+const motoRoutes = require('./routes/motoRoutes');
+
 app.use(express.json());
+app.use('/motos', motoRoutes);
 
 app.get('/', (req,res) => {
     res.send('Bem vindo a API.')
