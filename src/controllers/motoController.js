a0942aca170ac0f83271c8020dad8289b6f05572
Arthur Hipólito <hipolitoarthur3622@gmail.com>
2026-08-11 19:50:08 -0300

    feat: completa CRUD de motos

--- src/controllers/motoController.js
+++ src/controllers/motoController.js
@@ -34,6 +34,91 @@ const criarMoto = async (req, res) => {
     }
 };
 
-module.exports = {
-    criarMoto
+
+const listarMotos = async (req, res) => {
+    try {
+        const motosListadas = await prisma.moto.findMany();
+        res.json(motosListadas);
+    } catch (error) {
+        console.error(error);
+        res.send("Erro ao listar as motos!");
+    }
 };
+
+const buscarMotoPorID = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const moto = await prisma.moto.findUnique({
+            where: {
+                id: Number(id)
+            }
+        });
+        if (!moto) {
+            return res.status(404).send("Moto não encontrada.")
+        }
+        res.json(moto);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro ao buscar a moto.");
+    }
+};
+
+const atualizarMoto = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {
+            marca,
+            modelo,
+            quilometragem,
+            placa,
+            chassi,
+            cor,
+            cilindrada
+        } = req.body;
+
+        await prisma.moto.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                marca,
+                modelo,
+                quilometragem,
+                placa,
+                chassi,
+                cor,
+                cilindrada
+            }
+        });
+        res.send("Moto atualizada com sucesso!");
+    }
+    catch (error) {
+
+        console.error(error)
+        res.status(500).send("Erro ao atualizar a moto.");
+    }
+};
+
+const deletarMoto = async (req,res) => {
+    try{
+        const { id } = req.params;
+        await prisma.moto.delete({
+            where: {
+                id: Number(id)
+            }    
+        });
+        res.json("Moto deletada com sucesso!");
+
+    } catch(error){
+        console.error(error);
+        res.status(500).send("Erro ao deletar a moto.");
+    }
+}
+
+module.exports = {
+    criarMoto,
+    listarMotos,
+    buscarMotoPorID,
+    atualizarMoto,
+    deletarMoto
+};  
--- src/routes/motoRoutes.js
+++ src/routes/motoRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const motoController = require('../controllers/motoController');
 
-router.post('/', motoController.criarMoto)
+router.post('/', motoController.criarMoto);
+router.get('/', motoController.listarMotos);
+router.get('/:id', motoController.buscarMotoPorID);
+router.put('/:id', motoController.atualizarMoto);
+router.delete('/:id', motoController.deletarMoto);
 
 module.exports = router;
